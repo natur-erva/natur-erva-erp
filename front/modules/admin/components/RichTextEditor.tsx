d4a59fd710969635cbbeb4d4bc52e5ca1db0c319
Personalcreativelda <ervanatur@gmail.com>
2026-06-05 12:27:49 -0700

    frontand corrigido o deploy

--- front/modules/admin/components/RichTextEditor.tsx
+++ front/modules/admin/components/RichTextEditor.tsx
@@ -5,7 +5,7 @@ import Image from '@tiptap/extension-image';
 import Link from '@tiptap/extension-link';
 import TextAlign from '@tiptap/extension-text-align';
 import Underline from '@tiptap/extension-underline';
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyle } from '@tiptap/extension-text-style';
 import Color from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
 import FontFamily from '@tiptap/extension-font-family';
